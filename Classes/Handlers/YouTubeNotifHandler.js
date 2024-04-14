d27df5ef2c4f1d35fa6a317902a803ff7cae6db4
BiG ADLER <95148142+BiG-ADLER@users.noreply.github.com>
2024-04-14 12:49:06 +0330

    create YT Notification Handler

--- Classes/Handlers/YouTubeNotifHandler.js
+++ Classes/Handlers/YouTubeNotifHandler.js
@@ -1,5 +1,6 @@
 import Parser from "rss-parser";
 import YTNotify from "../../Schemas/YTNotify.js";
+import Messages from "../Messages/Messages.js"
 
 const parser = new Parser();
 
@@ -38,38 +39,75 @@ export default class YouTubeNotifHandler {
                                     await YTNotify.updateOne({ GuildID: data.GuildID, ID: data.ID }, { $push: { Latest: id } })
                                 }
 
-                                let PingRole = Setups[0].PingRole;
+                                let PingRole = data.PingRole;
                                 if (PingRole) {
-                                    if (PingRole === Setups[0].GuildID) PingRole = `@everyone`
-                                    else PingRole = `<@&${Setups[0].PingRole}>`
+                                    if (PingRole === data.GuildID) PingRole = `@everyone`
+                                    else PingRole = `<@&${data.PingRole}>`
                                 } else {
                                     PingRole = 'none';
                                 }
 
-                                if (Setups[0].Message) {
+                                if (data.Message) {
                                     if (PingRole !== 'none') {
-                                        // await
+                                        await ChannelID.send({content: `${PingRole} \n\n${data.Message.replace('{author}', author).replace('{title}', title).replace('{link}', link)}`});
+                                    } else {
+                                        await ChannelID.send({content: `**${author}** hamin alan video **${title}** ro Upload kard.\n\n${link}`});
                                     }
                                 }
 
                             } catch (error) {
-
+                                Messages.Error("YouTubeNotifHandler => Init -> One", error);
                             }
 
-                        })
+                        }, 5000)
 
                     }))
 
+                } else {
+                    try {
+                        let VideoData = await parser.parseURL(`https://www.youtube.com/feeds/videos.xml?channel_id=${Setups[0].ID}`);
+                        if (!VideoData) return;
+
+                        let GuildID = await client.guilds.cache.get(`${Setups[0].GuildID}`);
+                        if (!GuildID) return;
+
+                        let ChannelID = await guild.channels.cache.get(`${Setups[0].Channel}`);
+                        if (!ChannelID) return;
+
+                        let { link, author, title, id } = VideoData.items[0];
+                        if (Setups[0].Latest.includes(id)) return;
+                        else {
+                            await YTNotify.updateOne({ GuildID: Setups[0].GuildID, ID: Setups[0].ID }, { $push: { Latest: id } })
+                        }
+
+                        let PingRole = Setups[0].PingRole;
+                        if (PingRole) {
+                            if (PingRole === Setups[0].GuildID) PingRole = `@everyone`
+                            else PingRole = `<@&${Setups[0].PingRole}>`
+                        } else {
+                            PingRole = 'none';
+                        }
+
+                        if (Setups[0].Message) {
+                            if (PingRole !== 'none') {
+                                await ChannelID.send({content: `${PingRole} \n\n${Setups[0].Message.replace('{author}', author).replace('{title}', title).replace('{link}', link)}`});
+                            } else {
+                                await ChannelID.send({content: `${Setups[0].Message.replace('{author}', author).replace('{title}', title).replace('{link}', link)}`});
+                            }
+                        } else {
+                            if (PingRole !== 'none') {
+                                await ChannelID.send({content: `${PingRole} \n\n **${author}** Hamin alan video **${title}** ro upload kard.\n\n${link}`});
+                            } else {
+                                await ChannelID.send({content: `**${author}** Hamin alan video **${title}** ro upload kard.\n\n${link}`})
+                            }
+                        }
+                    } catch (error) {
+                        Messages.Error("YouTubeNotifHandler => Init -> Two", error);
+                    }
                 }
-
-
-
             }
-
         } catch (error) {
-
+            Messages.Error("YouTubeNotifHandler => Init", error);
         }
-
     }
-
 }
